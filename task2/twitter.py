c0b1975558d72ea7652211e56075957af81f42e2
Stein-Otto Svorstøl <steinotto.svorstol@gmail.com>
2016-04-24 15:25:49 +0200

    Use dictionary for checking if word is positve or negative
    User runtime 20 seconds

--- task2/twitter.py
+++ task2/twitter.py
@@ -8,25 +8,31 @@ import datetime
 import calendar
 from datetime import timedelta
 
-from pyspark import SparkContext
+from pyspark import SparkContext, SparkConf
 
 INPUT_DATA_PATH = sys.argv[1]
 OUTPUT_DATA_FILE = sys.argv[2]
 
-sc = SparkContext("local[*]", "TDT4305 Task 2: Twitter Analysis")
+conf = (SparkConf().setAppName("TDT4305 Task 2: Twitter Analysis"))
+sc = SparkContext(conf=conf)
 
 twitter_dataset = sc.textFile(INPUT_DATA_PATH + '/geotweets.tsv', use_unicode=False)
 
 positive_words = sc.textFile("hdfs://dascosa09.idi.ntnu.no:8020/user/janryb/positive-words.txt").collect()
 negative_words = sc.textFile("hdfs://dascosa09.idi.ntnu.no:8020/user/janryb/negative-words.txt").collect()
 
+LOOKUP_TABLE = {}
+for word in positive_words: LOOKUP_TABLE[word] = 1
+for word in negative_words: LOOKUP_TABLE[word] = -1
+
 def calculate_text_sentiment(text):
     sentiment = 0
     words = text.split()
     for word in words:
-        lower_word = word.lower()
-        if lower_word in positive_words: sentiment +=1
-        elif lower_word in negative_words: sentiment -=1
+        try:
+            sentiment += LOOKUP_TABLE[word.lower()]
+        except KeyError:
+            continue
     return sentiment
 
 def get_weekday(timestamp, offset):
@@ -36,14 +42,19 @@ def get_weekday(timestamp, offset):
     return calendar.day_name[local_time.weekday()]
 
 def add(a,b):
-    return a+b
+    return a +  b
+
+def pretty_print_row(row):
+    return row[0][0] + "\t" + row[0][1] +  "\t" + str(row[1])
 
 twitter_dataset \
     .map(lambda row: row.split("\t")) \
     .filter(lambda tweet: tweet[5] == "en"
                           and tweet[2] == "US"
                           and tweet[3] == "city") \
-    .map(lambda tweet: ((tweet[4], get_weekday(tweet[0], tweet[8])), calculate_text_sentiment(tweet[10]))) \
-    .combineByKey(int, add, add) \
-    .map(lambda row: row[0][0] + "\t" + row[0][1] +  "\t" + str(row[1]) ) \
+    .map(lambda tweet: ((tweet[4], get_weekday(tweet[0], tweet[8])), tweet[10])) \
+    .map(lambda row: (row[0], calculate_text_sentiment(row[1]))) \
+    .reduceByKey(add) \
+    .map(pretty_print_row) \
+    .coalesce(1) \
     .saveAsTextFile(OUTPUT_DATA_FILE)
